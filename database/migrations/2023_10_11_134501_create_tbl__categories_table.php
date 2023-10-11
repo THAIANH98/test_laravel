e931b64f01f4ed2a842bd78907537dc750e34e0b
thaianhtran98 <thaianhtran2311@gmail.com>
2023-10-11 20:55:02 +0700

    tạo bảng
    tạo 2 bảng và model

---
+++ database/migrations/2023_10_11_134501_create_tbl__categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl__categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('ma_nhom');
+            $table->string('ten_nhom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl__categories');
+    }
+};
---
+++ database/migrations/2023_10_11_134555_create_tbl__products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl__products', function (Blueprint $table) {
+            $table->id();
+            $table->string('ma_sp');
+            $table->string('ten_sp');
+            $table->string('donvi_sp');
+            $table->string('gia_sp');
+            $table->string('ten_sp');
+            $table->unsignedBigInteger('id_nhom');
+            $table->foreign('id_nhom')
+            ->references('id')
+            ->on('tbl__categories')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl__products');
+    }
+};
